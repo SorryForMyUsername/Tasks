be7f6c9e8b65f6fca512b2b40f9be1b5a3dfde12
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-11-12 13:42:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func(-5, 5, 2);
+
+            Console.ReadKey();
+        }
+
+        public static void Func(int startValue, int endValue, int step)
+        {
+            for(int x = startValue; x <= endValue; x += step)
+            {
+                double y = 5 * Math.Pow(x, 2) - 2 * x + 1;
+                Console.WriteLine($"y({x}) = {y}");
+            }
+        }
+    }
+}
