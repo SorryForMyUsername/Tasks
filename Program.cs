66a02d16ab729a1c353d07c04946c89bb7a08d22
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-11-12 14:07:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EvenTwoDigitNumbers();
+
+            int x = NewInteger();
+
+            Console.ReadKey();
+        }
+
+        public static void EvenTwoDigitNumbers()
+        {
+            int count = 0;
+            for(int twoDigitNumber = 10; twoDigitNumber < 100; twoDigitNumber++)
+            {
+                if(twoDigitNumber % 2 == 0)
+                {
+                    Console.WriteLine(twoDigitNumber);
+                    count++;
+                }
+            }
+            Console.WriteLine($"Кол-во чётных двухзначных чисел: {count}");
+        }
+
+        public static int NewInteger()
+        {
+            Console.Write("Значение новой переменной: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
